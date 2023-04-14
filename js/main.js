22f3b56619fe14c70b37d189a8723436e10ade1f
ssuk80 <xkftjs@gmail.com>
2023-04-14 18:30:05 +0900

    비주얼영역

--- js/main.js
+++ js/main.js
@@ -33,16 +33,49 @@ window.addEventListener('scroll', function () {
   //  그렇지않으면 다시 보이기!
   
   if (window.scrollY > 500 ) {
-    // 배지요소 숨김
-    badgeEl.style.display = 'none';
+  // 배지요소 숨김
+  // badgeEl.style.display = 'none';
+
+  //  gsap.to (요소,지속시간, 옵션: {}) 메소드 : css 속성을 통해 애니메이션 처리
+    gsap.to(badgeEl,0.6,{
+      opacity:0,
+      display:'none'
+    });
   } else {
     // 배지요소 보이기  
-    badgeEl.style.display = 'block';
-}
+    // badgeEl.style.display = 'block';
 
+    gsap.to(badgeEl,0.6,{
+      opacity:1,
+      display:'block'
+    });
+  }
 });
 
-
+// 라이브러리가 애니메이션처리가 되게끔 디스플레이 블럭이나 논이 안되는걸(속성에 안된다고 나옴) 트렌지션 해줌
 
   
 
+// 순차적으로 visual 섹션 내요소 보이기
+// 나타날 요소 (.fade-in)들을 찾기
+const fadeEls = document.querySelectorAll ('.visual .fade-in');
+
+// 요소들을 하나씩 반복해서 처리
+fadeEls.forEach(function (fadeEl, index) {
+  //  gsap.to (요소,지속시간, 옵션은 여기안에{}) 메소드는 : css 속성을 통해 애니메이션 처리
+  gsap.to(fadeEl, 1, {
+    // delay : 몇 초뒤에 실행 될 것인가?
+    delay: (index + 1) * 0.7, // 0.7, 1.4 2.1 2.8초
+    opacity:1
+  });
+});
+
+
+//공지사항 수직 슬라이드 기능 작성
+//new 키워드를 Swiper 객체를 생성 -> 슬라이드 기능을 생성
+// new.Swiper (선택자 , 옵션: {});
+new Swiper ('.notice .swiper', {
+  direction:'vertical',//수직 슬라이드
+  loop: true,//반복 재생 여부
+  autoplay: true, //자동재성ㅇ부
+});
